2514cca4f26637584475145dcf3f5cc6772063ca
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-02-24 03:01:32 +0330

    catch all route

--- pages/articles/[...slugs].js
+++ pages/articles/[...slugs].js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 function Article() {
+  const router = useRouter();
+  const slugs = router.query.slugs || [];
+
+  if (slugs.length > 1) {
+    return (
+      <h3>
+        this article was written by {slugs[0]}at {slugs[1]}
+      </h3>
+    );
+  }
   return <h1>Article</h1>;
 }
 
