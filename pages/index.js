7f933be8044aa7f5e9de40550f2dd0a6e703ac30
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-02-25 00:11:45 +0330

    link component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -6,6 +8,10 @@ export default function Home() {
   return (
     <>
       <h1>hello world</h1>
+      <ul>
+        <li><Link href='/courses'>go to courses</Link></li>
+        <li><Link href='/users'>go to users</Link></li>
+      </ul>
     </>
-  );
+  )
 }
