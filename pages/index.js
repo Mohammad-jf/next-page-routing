288d06b07dbae736210a98406966c27c2952d2a1
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-02-25 01:56:13 +0330

    navigate

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,31 @@
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
-
+import { useRouter } from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const router = useRouter();
+
+  const clickHandler = (e) => {
+    //logging process
+    router.push('/students');
+  };
+
   return (
     <>
       <h1>hello world</h1>
       <ul>
-        <li><Link href='/courses'>go to courses</Link></li>
-        <li><Link href='/users'>go to users</Link></li>
+        <li>
+          <Link href='/courses'>go to courses</Link>
+        </li>
+        <li>
+          <Link href='/users'>go to users</Link>
+        </li>
+        <li>
+          <button onClick={clickHandler}>LogIn</button>
+        </li>
       </ul>
     </>
-  )
+  );
 }
