adb09c774c988a3e2fec86d46ba71a36c63bc3e3
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-02-25 00:25:23 +0330

    dynamic routing

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 function Users() {
+  const users = [
+    {
+      id: 1,
+      name: 'user1',
+    },
+    {
+      id: 2,
+      name: 'user2',
+    },
+    {
+      id: 3,
+      name: 'user3',
+    },
+    {
+      id: 4,
+      name: 'user4',
+    },
+  ];
+
   return (
-    <div>index</div>
-  )
+    <>
+      <div>Users</div>
+      {users.map((user) => {
+        return (
+          <h1 key={user.id}>
+            <Link href={`/users/${user.id}`}>{user.name}</Link>
+          </h1>
+        );
+      })}
+    </>
+  );
 }
 
-export default Users
+export default Users;
