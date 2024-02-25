6fb8d2ecc0b95e2987c4395a9ffa6a5dc40fa2a0
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-02-25 04:38:34 +0330

    style-jsx

--- pages/courses/react.js
+++ pages/courses/react.js
@@ -1,5 +1,19 @@
 function React() {
-  return <div>react</div>;
+  return (
+    <>
+      <h1>React</h1>
+      <p>you should learn react before next js</p>
+      <h4>mohammad jalifathi</h4>
+      <style jsx>
+        {`
+          h1 {
+            color: red;
+            background-color: green;
+          }
+        `}
+      </style>
+    </>
+  );
 }
 
 export default React;
